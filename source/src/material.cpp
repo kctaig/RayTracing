487a6a3557174e3a88441d27610e00e5ed67aabf
liyangxu <kctaig@qq.com>
2025-03-27 21:17:28 +0800

    fix texture mapping

--- source/src/material.cpp
+++ source/src/material.cpp
@@ -11,13 +11,13 @@ Texture::Texture(const string& path, const string& texName)
 	else std::cout << "Successfully Loaded texture: " << texName << std::endl;
 }
 
-vec3 Texture::value(const vec2 &texCoord) const
+vec3 Texture::value(const vec2& texCoord) const
 {
 	float u = glm::clamp(texCoord.x, 0.f, 1.f);
 	float v = glm::clamp(texCoord.y, 0.f, 1.f);
 	int x = static_cast<int>(u * width);
 	int y = static_cast<int>(v * height);
-	int offset = (y * width + x) * channels;
+	int offset = ((height - y) * width + x) * channels;
 	offset = glm::clamp(offset, 0, width * height * channels - 3);
 	return vec3(data[offset], data[offset + 1], data[offset + 2]);
 }
